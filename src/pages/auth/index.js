0b0a0b87d8b7c20e95be24dd6534becc1a72c746
abhijeet-steppingedge <abhijeet@steppingedge.com>
2026-01-30 18:33:50 +0530

    Correction in policy url

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -195,11 +195,11 @@ const AuthPage = () => {
             >
               <p className='w-full sm:w-[341.99px]'>
                 {t('auth_page.terms')}{' '}
-                <a href='https://sst.vision/terms-of-use' target='_blank' className={`${textcolors.link}`}>
+                <a href='https://360vision.ai/terms-of-use' target='_blank' className={`${textcolors.link}`}>
                   {t('auth_page.terms_of_service')}
                 </a>{' '}
                 {t('auth_page.and')}{' '}
-                <a href='https://sst.vision/privacy-policy' target='_blank' className={`${textcolors.link}`}>
+                <a href='https://360vision.ai/privacy-policy' target='_blank' className={`${textcolors.link}`}>
                   {t('auth_page.privacy_policy')}
                 </a>
               </p>
